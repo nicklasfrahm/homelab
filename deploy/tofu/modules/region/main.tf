dfb3db9c2ce985f709aeb884cd4918eea04ed4bc
Nicklas Frahm <nicklas.frahm@gmail.com>
2025-02-10 23:08:56 +0100

    feat(talos): fetch Talos version

--- deploy/tofu/modules/region/main.tf
+++ deploy/tofu/modules/region/main.tf
@@ -1,8 +1,26 @@
 locals {
-  name = var.config.metadata.name
+  name = var.region.metadata.name
 }
 
-resource "local_file" "config" {
-  filename = "${path.cwd}/deploy/tofu/out/${local.name}.yaml"
-  content  = yamlencode(var.config)
+# Get the latest Talos version from image factory.
+data "talos_image_factory_versions" "this" {
+  filters = {
+    stable_versions_only = true
+  }
+}
+
+# Extract the latest Talos version.
+locals {
+  talos_version = element(data.talos_image_factory_versions.this.talos_versions, length(data.talos_image_factory_versions.this.talos_versions) - 1)
+}
+
+# Create the Talos secret bundle.
+resource "talos_machine_secrets" "this" {
+  talos_version = local.talos_version
+}
+
+# Create a file with the Talos version.
+resource "local_file" "version" {
+  filename = "${path.cwd}/deploy/tofu/out/${local.name}"
+  content = local.talos_version
 }
--- deploy/tofu/modules/region/variables.tf
+++ deploy/tofu/modules/region/variables.tf
@@ -1,11 +1,47 @@
-variable "config" {
+variable "region" {
+  description = "The configuration of the region."
   type = object({
     metadata = object({
       name = string
     })
     spec = object({
       provider = string
+      baremetal = object({
+        controlplanes = list(object({
+          name = string
+        }))
+      })
     })
   })
-  description = "The configuration of the region."
+}
+
+variable "machines" {
+  description = "The configuration of all machines."
+  type = map(object({
+    metadata = object({
+      name = string
+    })
+    spec = object({
+      hardware = object({
+        vendor = string
+        model = string
+      })
+      interfaces = list(object({
+        mac = string
+      }))
+    })
+  }))
+}
+
+variable "hardware_profiles" {
+  description = "The configuration of all hardware profiles."
+  type = map(object({
+    spec = object({
+      storage = object({
+        osDisk = object({
+          name = string
+        })
+      })
+    })
+  }))
 }
