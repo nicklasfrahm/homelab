bec19d3c9351552fff553df92fd3a7bffd8ce307
Nicklas Frahm <nicklas.frahm@gmail.com>
2025-02-10 20:06:29 +0100

    fix(tofu): avoid broken local plan

--- deploy/tofu/modules/region/main.tf
+++ deploy/tofu/modules/region/main.tf
@@ -1,3 +1,8 @@
-resource "kind_cluster" "test" {
+locals {
   name = var.config.metadata.name
+}
+
+resource "local_file" "config" {
+  filename = "${path.cwd}/deploy/tofu/out/${local.name}.yaml"
+  content  = yamlencode(var.config)
 }
--- deploy/tofu/modules/region/providers.tf
+++ deploy/tofu/modules/region/providers.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1.9.0"
 
   required_providers {
-    kind = {
-      source = "tehcyx/kind"
-      version = "0.7.0"
-    }
     local = {
       source = "hashicorp/local"
       version = "2.5.2"
